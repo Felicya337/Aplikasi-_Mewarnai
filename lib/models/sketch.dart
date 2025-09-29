1caa825579405715ff4a98f3d784226f50855eb2
Christina-Sitinjak <sitinjakchristina959@gmail.com>
2025-09-29 08:51:00 +0700

    done

---
+++ lib/models/sketch.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Sketch {
+  final List<Offset> points;
+  final Color color;
+
+  Sketch({required this.points, required this.color});
+}
--- lib/widgets/coloring_canvas.dart
+++ lib/widgets/coloring_canvas.dart
@@ -1,3 +1,4 @@
+import 'package:aplikasi_mewarnai/models/sketch.dart';
 import 'package:flutter/material.dart';
 
 class ColoringCanvas extends StatefulWidget {
@@ -15,37 +16,48 @@ class ColoringCanvas extends StatefulWidget {
 }
 
 class ColoringCanvasState extends State<ColoringCanvas> {
-  List<Offset> points = <Offset>[];
+  List<Sketch> sketches = <Sketch>[];
+  List<Offset> currentPoints = <Offset>[]; // Daftar titik untuk goresan saat ini
 
   void clearDrawing() {
     setState(() {
-      points.clear();
+      sketches.clear();
+      currentPoints.clear();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onPanStart: (details) { // Tambahkan onPanStart
+        setState(() {
+          currentPoints = <Offset>[]; // Mulai goresan baru
+        });
+      },
       onPanUpdate: (DragUpdateDetails details) {
         setState(() {
           RenderBox box = context.findRenderObject() as RenderBox;
           Offset localPosition = box.globalToLocal(details.globalPosition);
-          points = List.from(points)..add(localPosition);
+          currentPoints = List.from(currentPoints)..add(localPosition); // Tambahkan titik ke goresan saat ini
         });
       },
-      onPanEnd: (DragEndDetails details) => points.add(Offset.infinite),
+      onPanEnd: (DragEndDetails details) {
+        setState(() {
+          sketches = List.from(sketches)..add(Sketch(points: currentPoints, color: widget.selectedColor)); // Tambahkan goresan ke daftar goresan dengan warna yang dipilih
+          currentPoints = <Offset>[]; // Reset daftar titik untuk goresan berikutnya
+        });
+      },
       child: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage(widget.imagePath),
-            fit: BoxFit.cover, // biar pas penuh
+            fit: BoxFit.cover,
           ),
         ),
         child: CustomPaint(
           size: Size.infinite,
           painter: SketchPainter(
-            points: points,
-            selectedColor: widget.selectedColor,
+            sketches: sketches,
           ),
         ),
       ),
@@ -54,31 +66,30 @@ class ColoringCanvasState extends State<ColoringCanvas> {
 }
 
 class SketchPainter extends CustomPainter {
-  final List<Offset> points;
-  final Color selectedColor;
+  final List<Sketch> sketches;
 
   SketchPainter({
-    required this.points,
-    required this.selectedColor,
+    required this.sketches,
   });
 
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = selectedColor
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 5.0;
+    for (final sketch in sketches) {
+      Paint paint = Paint()
+        ..color = sketch.color
+        ..strokeCap = StrokeCap.round
+        ..strokeWidth = 5.0;
 
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != Offset.infinite && points[i + 1] != Offset.infinite) {
-        canvas.drawLine(points[i], points[i + 1], paint);
+      for (int i = 0; i < sketch.points.length - 1; i++) {
+        if (sketch.points[i] != Offset.infinite && sketch.points[i + 1] != Offset.infinite) {
+          canvas.drawLine(sketch.points[i], sketch.points[i + 1], paint);
+        }
       }
     }
   }
 
   @override
   bool shouldRepaint(SketchPainter oldDelegate) {
-    return oldDelegate.points != points ||
-        oldDelegate.selectedColor != selectedColor;
+    return oldDelegate.sketches != sketches;
   }
 }
